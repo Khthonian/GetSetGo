2deff42c7447b59f096564cf8446d33d1d1ed7dc
Khthonian <anansicorp@tuta.io>
2023-08-24 00:02:10 +0100

    feat(src): Allow install selection
    The script now gets the user to select the software they want to
    install. This is much safer as it allows for the script to be grown
    indefinitely, otherwise a user could easily install too much or unwanted
    software. Input validations still need to be improved.

--- getSetGo.sh
+++ getSetGo.sh
@@ -1,43 +1,67 @@
 #!/bin/bash
 
-# Function to update and upgrade system
+# Define a function to update and upgrade system
 update_system() {
+    #
     sudo apt update && sudo apt upgrade -y || {
         echo "Update failed"
         exit 1
     }
 }
 
-# Function to install common tools
-install_common_tools() {
-    sudo apt install -y curl wget build-essential || {
-        echo "Common tools installation failed"
+# Define a function to install curl
+install_curl() {
+    sudo apt install -y curl || {
+        echo "Curl installation failed"
         exit 1
     }
 }
 
-# Function to install Git
+# Define a function to install wget
+install_wget() {
+    sudo apt install -y wget || {
+        echo "Wget installation failed"
+        exit 1
+    }
+}
+
+# Define a function to install build-essentials
+install_build_essentials() {
+    sudo apt install -y build-essential || {
+        echo "Build-essentials installation failed"
+        exit 1
+    }
+}
+
+# Define a function to install Git fro the Git-core PPA
 install_git() {
     sudo add-apt-repository -y ppa:git-core/ppa
     sudo apt update
-    sudo apt install -y git || {
+    sudo apt install -y git
+    if [ $? -eq 0 ]; then
+        echo "Git installation successful"
+        configure_git
+    else
         echo "Git installation failed"
         exit 1
-    }
+    fi
 }
 
-# Function to install Zsh
+# Define a function to install Zsh and switch the default shell for the user who initiates the script
 install_zsh() {
     sudo apt install -y zsh || {
         echo "Zsh installation failed"
         exit 1
     }
+    # Determine the user who ran the script
     if [ ! -z "$SUDO_USER" ]; then
         user="$SUDO_USER"
     else
         user="$(whoami)"
     fi
+    # Get the current shell for the user
     user_shell=$(getent passwd $user | cut -d: -f7)
+    # Switch the default user shell to Zsh
     chsh -s $(which zsh) $user || {
         echo "Changing default shell to Zsh failed"
         exit 1
@@ -45,7 +69,7 @@ install_zsh() {
     echo "Default shell for $user changed from $user_shell to $(which zsh)"
 }
 
-# Function to install Kitty Terminal
+# Define a function to install Kitty terminal
 install_kitty() {
     sudo apt install -y kitty || {
         echo "Kitty installation failed"
@@ -53,7 +77,7 @@ install_kitty() {
     }
 }
 
-# Function to install Python
+# Define a function to install Python 3 with pip
 install_python() {
     sudo apt install -y python3 python3-pip || {
         echo "Python installation failed"
@@ -61,15 +85,16 @@ install_python() {
     }
 }
 
-# Function to install Lua
+# Define a function to install Lua
 install_lua() {
+    # TODO: Update the version when necessary
     sudo apt install -y lua5.3 || {
         echo "Lua installation failed"
         exit 1
     }
 }
 
-# Function to install Go
+# Define a function to install Go
 install_go() {
     sudo apt install -y golang-go || {
         echo "Go installation failed"
@@ -77,7 +102,7 @@ install_go() {
     }
 }
 
-# Function to install Rust
+# Define a function to install Rust
 install_rust() {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh || {
         echo "Rust installation failed"
@@ -85,7 +110,7 @@ install_rust() {
     }
 }
 
-# Function to install GCC
+# Define a function to install GCC compiler
 install_gcc() {
     sudo apt install -y gcc || {
         echo "GCC installation failed"
@@ -93,14 +118,16 @@ install_gcc() {
     }
 }
 
-# Function to install Anaconda
+# Define a function to install Anaconda
 install_anaconda() {
+    # TODO: Update the version when necessary
     wget https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
     bash Anaconda3-2023.07-2-Linux-x86_64.sh
     rm Anaconda3-2023.07-2-Linux-x86_64.sh || {
         echo "Anaconda installation failed"
         exit 1
     }
+    # Update Anaconda
     source ~/anaconda3/bin/activate
     conda update -y --all || {
         echo "Anaconda update failed"
@@ -108,16 +135,18 @@ install_anaconda() {
     }
 }
 
-# Function to ask the user for Git configurations
+# Define a function to ask the user for Git configurations
 configure_git() {
     read -p "Enter your name for Git user.name: " git_username
     read -p "Enter your email for Git user.email: " git_email
 
+    # Validate the configuration inputs
     if [[ -z "$git_username" || -z "$git_email" ]]; then
         echo "Git username or email cannot be empty."
         exit 1
     fi
 
+    # Configure Git with user inputs
     git config --global user.name "$git_username"
     git config --global user.email "$git_email"
     git config --global init.defaultBranch main
@@ -128,7 +157,7 @@ configure_git() {
     }
 }
 
-# Function to install Edge
+# Define a function to install Microsoft Edge browser
 install_edge() {
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
     sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -141,7 +170,7 @@ install_edge() {
     }
 }
 
-# Function to install Discord
+# Define a function to install Discord
 install_discord() {
     wget -O discord.deb "https://discord.com/api/download?platform=linux&format=deb"
     sudo dpkg -i discord.deb
@@ -152,7 +181,7 @@ install_discord() {
     }
 }
 
-# Function to install Bitwarden
+# Define a function to install Bitwarden
 install_bitwarden() {
     wget -O bitwarden.deb "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb"
     sudo dpkg -i bitwarden.deb
@@ -163,7 +192,7 @@ install_bitwarden() {
     }
 }
 
-# Function to install Spotify
+# Define a function to install Spotify
 install_spotify() {
     curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -174,7 +203,7 @@ install_spotify() {
     }
 }
 
-# Function to install VS Code
+# Define a function to install Visual Studio Code
 install_vscode() {
     sudo apt install -y software-properties-common apt-transport-https curl
     curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
@@ -186,40 +215,58 @@ install_vscode() {
     }
 }
 
-# Function to install Vim
+# Define a function to install Vim
 install_vim() {
-  sudo apt install -y vim || { echo "Vim installation failed"; exit 1; }
+    sudo apt install -y vim || {
+        echo "Vim installation failed"
+        exit 1
+    }
 }
 
-# Function to install Neovim
+# Define a function to install Neovim
 install_neovim() {
-  sudo apt install -y neovim || { echo "NeoVim installation failed"; exit 1; }
+    sudo apt install -y neovim || {
+        echo "NeoVim installation failed"
+        exit 1
+    }
 }
 
-# Function to install Firefox
+# Define a function to install Mozilla Firefox browser
 install_firefox() {
-  sudo apt install -y firefox || { echo "Firefox installation failed"; exit 1; }
+    sudo apt install -y firefox || {
+        echo "Firefox installation failed"
+        exit 1
+    }
 }
 
-# Function to install Google Chrome
+# Define a function to install Google Chrome browser
 install_chrome() {
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  sudo dpkg -i google-chrome-stable_current_amd64.deb
-  sudo apt install -fy
-  rm google-chrome-stable_current_amd64.deb || { echo "Google Chrome installation failed"; exit 1; }
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo apt install -fy
+    rm google-chrome-stable_current_amd64.deb || {
+        echo "Google Chrome installation failed"
+        exit 1
+    }
 }
 
-# Function to install tmux
+# Define a function to install tmux multiplexer
 install_tmux() {
-  sudo apt install -y tmux || { echo "tmux installation failed"; exit 1; }
+    sudo apt install -y tmux || {
+        echo "tmux installation failed"
+        exit 1
+    }
 }
 
-# Function to install htop
+# Define a function to install htop process viewer
 install_htop() {
-  sudo apt install -y htop || { echo "htop installation failed"; exit 1; }
+    sudo apt install -y htop || {
+        echo "htop installation failed"
+        exit 1
+    }
 }
 
-# Function to install nvm and Node
+# Define a function to install nvm and Node
 install_node() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -231,27 +278,98 @@ install_node() {
     }
 }
 
-# Main function to call all the other functions
+# Define a function to prompt user for selection within a category
+select_from_category() {
+    local category_name="$1"
+    declare -n options="$2"
+    local selections=()
+
+    # Print the options + the "Skip category" option
+    echo -e "\nSelect from the $category_name category:"
+    local i=1
+    for key in "${!options[@]}"; do
+        echo "$i) $key"
+        i=$((i + 1))
+    done
+    echo "$i) Skip category"
+
+    read -p "Enter your choices (comma-separated, e.g., 1,2,3): " user_choices
+    IFS=',' read -ra selections <<<"$user_choices"
+
+    # Check for the "Skip category" option
+    if [[ " ${selections[*]} " == *" $i "* ]]; then
+        echo "Skipping $category_name category."
+        return
+    fi
+
+    # Execute and install the selected options
+    for selection in "${selections[@]}"; do
+        index=$((selection - 1))
+        if [ "$index" -ge 0 ] && [ "$index" -lt "${#options[@]}" ]; then
+            eval "${options[$index]}"
+        fi
+    done
+}
+
+# Main function to guide user through category selections
 main() {
     update_system
-    install_common_tools
-    install_git
-    install_zsh
-    install_kitty
-    install_python
-    install_lua
-    install_go
-    install_rust
-    install_gcc
-    install_anaconda
-    configure_git
-    install_edge
-    install_discord
-    install_bitwarden
-    install_spotify
-    install_vscode
-    install_node
 
+    declare -A common_tools=(
+        ["Curl"]="install_curl"
+        ["Wget"]="install_wget"
+        ["Build-essentials"]="install_build_essentials"
+    )
+    select_from_category "Common Tools" common_tools
+
+    declare -A terminal_tools=(
+        ["Kitty"]="install_kitty"
+        ["Zsh"]="install_zsh"
+    )
+
+    declare -A dev_tools=(
+        ["Git"]="install_git"
+        ["GCC"]="install_gcc"
+        ["Anaconda"]="install_anaconda"
+        ["Node.js"]="install_node"
+    )
+    select_from_category "Development Tools" dev_tools
+
+    declare -A languages=(
+        ["Python"]="install_python"
+        ["Lua"]="install_lua"
+        ["Go"]="install_go"
+        ["Rust"]="install_rust"
+    )
+    select_from_category "Languages" languages
+
+    declare -A text_editors=(
+        ["VS Code"]="install_vscode"
+        ["Vim"]="install_vim"
+        ["NeoVim"]="install_neovim"
+    )
+    select_from_category "Text Editors" text_editors
+
+    declare -A browsers=(
+        ["Microsoft Edge"]="install_edge"
+        ["Mozilla Firefox"]="install_firefox"
+        ["Google Chrome"]="install_chrome"
+    )
+    select_from_category "Web Browsers" browsers
+
+    declare -A media=(
+        ["Discord"]="install_discord"
+        ["Spotify"]="install_spotify"
+    )
+
+    declare -A utilities=(
+        ["tmux"]="install_tmux"
+        ["htop"]="install_htop"
+        ["Bitwarden"]="install_bitwarden"
+    )
+    select_from_category "Utilities" utilities
+
+    configure_git
     echo "Installation complete!"
 }
 
